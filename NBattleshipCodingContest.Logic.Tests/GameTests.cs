e045c698b59ecf392e7e718f7d0cbf6b80fe936f
Rainer Stropek <rainer@software-architects.at>
2021-02-15 14:50:52 +0100

    Add game (with records)

---
+++ NBattleshipCodingContest.Logic.Tests/GameTests.cs
@@ -0,0 +1,176 @@
+﻿namespace NBattleshipCodingContest.Logic.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+
+    public class GameTests
+    {
+        private static Game CreateGame() =>
+            new Game(Guid.Empty, new[] { 47, 11 }, new[] { new BoardContent(SquareContent.Water), new BoardContent(SquareContent.Water) },
+                new[] { new BoardContent(SquareContent.Unknown), new BoardContent(SquareContent.Unknown) });
+
+        [Fact]
+        public void Shoot_Into_Water()
+        {
+            var shooterBoard = new BoardContent(SquareContent.Unknown);
+
+            // Note with expression for record. See also
+            // https://devblogs.microsoft.com/dotnet/welcome-to-c-9-0/#with-expressions
+
+            var game = CreateGame() with { ShootingBoards = new[] { shooterBoard, new BoardContent(SquareContent.Unknown) } };
+
+            Assert.Equal(SquareContent.Water, game.Shoot(1, "A1"));
+            Assert.Equal(SquareContent.Water, shooterBoard[new BoardIndex(0, 0)]);
+        }
+
+        [Fact]
+        public void Shoot_Ship()
+        {
+            var board = new BoardContent(SquareContent.Water);
+            board[0] = board[1] = SquareContent.Ship;
+            var shooterBoard = new BoardContent(SquareContent.Unknown);
+
+            var game = CreateGame() with
+            {
+                Boards = new[] { board, new BoardContent(SquareContent.Water) },
+                ShootingBoards = new[] { new BoardContent(SquareContent.Unknown), shooterBoard }
+            };
+
+            Assert.Equal(SquareContent.HitShip, game.Shoot(2, "A1"));
+            Assert.Equal(SquareContent.HitShip, shooterBoard[new BoardIndex(0, 0)]);
+        }
+
+        [Fact]
+        public void Sink_Ship()
+        {
+            var board = new BoardContent(SquareContent.Water);
+            board[0] = board[1] = SquareContent.Ship;
+            var shooterBoard = new BoardContent(SquareContent.Unknown);
+
+            var game = CreateGame() with
+            {
+                Boards = new[] { board, new BoardContent(SquareContent.Water) },
+                ShootingBoards = new[] { new BoardContent(SquareContent.Unknown), shooterBoard }
+            };
+
+            Assert.Equal(SquareContent.HitShip, game.Shoot(2, "A1"));
+            Assert.Equal(SquareContent.SunkenShip, game.Shoot(2, "B1"));
+            Assert.Equal(SquareContent.SunkenShip, shooterBoard[new BoardIndex(0)]);
+            Assert.Equal(SquareContent.SunkenShip, shooterBoard[new BoardIndex(1)]);
+        }
+
+        [Fact]
+        public void Shoot_Invalid()
+        {
+            var game = new Game(Guid.Empty, new[] { 0 }, new[] { new BoardContent(SquareContent.Water) },
+                new[] { new BoardContent(SquareContent.Unknown) });
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => game.Shoot(0, new BoardIndex()));
+            Assert.Throws<InvalidOperationException>(() => game.Shoot(1, new BoardIndex()));
+        }
+
+        [Fact]
+        public void GetShotRequest()
+        {
+            var game = CreateGame();
+            var shotRequest = game.GetShotRequest(1);
+
+            // Note value-based equality here. See also
+            // https://devblogs.microsoft.com/dotnet/welcome-to-c-9-0/#value-based-equality
+
+            Assert.Equal(new ShotRequest(Guid.Empty, 47, 11, game.ShootingBoards[0]), shotRequest);
+        }
+
+        [Fact]
+        public void GetWinner_Draw()
+        {
+            var shootingBoards = new[] { new BoardContent(SquareContent.Unknown), new BoardContent(SquareContent.Unknown) };
+            var game = CreateGame() with { ShootingBoards = shootingBoards };
+            shootingBoards[0][new BoardIndex(0, 0)] = shootingBoards[1][new BoardIndex(0, 0)] = SquareContent.HitShip;
+            Assert.Equal(Winner.Draw, game.GetWinner(1));
+        }
+
+        [Fact]
+        public void GetWinner_Draw_Too_Many_moves()
+        {
+            var shootingBoards = new[] { new BoardContent(SquareContent.Unknown), new BoardContent(SquareContent.Unknown) };
+            var game = CreateGame() with { ShootingBoards = shootingBoards };
+            for (var i = 0; i < 200; i++)
+            {
+                game.Shoot(1, new BoardIndex(0));
+            }
+
+            Assert.Equal(Winner.Draw, game.GetWinner(1));
+        }
+
+        [Fact]
+        public void GetWinner_Player1()
+        {
+            var shooterBoard1 = new BoardContent(SquareContent.Unknown);
+            var game = CreateGame() with { ShootingBoards = new[] { shooterBoard1, new BoardContent(SquareContent.Unknown) } };
+            shooterBoard1[new BoardIndex(0, 0)] = SquareContent.HitShip;
+            Assert.Equal(Winner.Player1, game.GetWinner(1));
+        }
+
+        [Fact]
+        public void GetWinner_Player2()
+        {
+            var shooterBoard2 = new BoardContent(SquareContent.Unknown);
+            var game = CreateGame() with { ShootingBoards = new[] { new BoardContent(SquareContent.Unknown), shooterBoard2 } };
+            shooterBoard2[new BoardIndex(0, 0)] = SquareContent.HitShip;
+            Assert.Equal(Winner.Player2, game.GetWinner(1));
+        }
+
+        [Fact]
+        public void GetWinner_NoWinner()
+        {
+            Assert.Equal(Winner.NoWinner, CreateGame().GetWinner(1));
+        }
+
+        [Fact]
+        public void Log()
+        {
+            var game = CreateGame();
+            game.Shoot(1, "A1");
+            game.Shoot(2, "A1");
+
+            Assert.Equal(2, game.Log.Count());
+            Assert.Equal(47, game.Log.First().Shooter);
+            Assert.Equal(new BoardIndex(), game.Log.First().Location);
+        }
+
+        [Fact]
+        public void LastShot()
+        {
+            var game = CreateGame();
+
+            var request = game.GetShotRequest(1);
+            Assert.Null(request.LastShot);
+            game.Shoot(1, "A1");
+            Assert.Equal("A1", game.GetLastShot(1));
+
+            request = game.GetShotRequest(2);
+            Assert.Null(request.LastShot);
+            game.Shoot(2, "A1");
+            Assert.Equal("A1", game.GetLastShot(2));
+
+            request = game.GetShotRequest(1);
+            Assert.NotNull(request.LastShot);
+            Assert.Equal("A1", request.LastShot!);
+            game.Shoot(1, "B1");
+            Assert.Equal("B1", game.GetLastShot(1));
+
+            request = game.GetShotRequest(2);
+            Assert.NotNull(request.LastShot);
+            Assert.Equal("A1", request.LastShot!);
+            game.Shoot(2, "B1");
+            Assert.Equal("B1", game.GetLastShot(2));
+
+            request = game.GetShotRequest(1);
+            Assert.NotNull(request.LastShot);
+            Assert.Equal("B1", request.LastShot!);
+        }
+    }
+}
---
+++ NBattleshipCodingContest.Logic/Game.cs
@@ -0,0 +1,198 @@
+﻿namespace NBattleshipCodingContest.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public enum Winner
+    {
+        NoWinner = -1,
+        Draw = 0,
+        Player1 = 1,
+        Player2 = 2
+    }
+
+    // Note use of records here. Read more at
+    // https://devblogs.microsoft.com/dotnet/welcome-to-c-9-0/#records
+
+    // Note limitations regarding XML documentation of records.
+
+    /// <summary>
+    /// Represents a record in the game log.
+    /// </summary>
+    public record GameLogRecord(int Shooter, BoardIndex Location, SquareContent ShotResult, string? ShooterName = null);
+
+    /// <summary>
+    /// Represents the state of a game between two players
+    /// </summary>
+    public record Game(Guid GameId, int[] PlayerIndexes, IReadOnlyBoard[] Boards, BoardContent[] ShootingBoards)
+    {
+        private readonly IList<GameLogRecord> log = new List<GameLogRecord>();
+
+        private static void EnsureValidShooter(int shootingPlayer)
+        {
+            if (shootingPlayer is < 1 or > 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(shootingPlayer), "Player must be 1 or 2");
+            }
+        }
+
+        private void EnsureValidBoards()
+        {
+            if (Boards.Length != 2 || ShootingBoards.Length != 2)
+            {
+                throw new InvalidOperationException("Board array(s) of wrong length");
+            }
+        }
+
+        /// <summary>
+        /// Gets the history of shots
+        /// </summary>
+        public IEnumerable<GameLogRecord> Log => log.ToArray();
+
+        /// <summary>
+        /// Given player shoots at a given index
+        /// </summary>
+        /// <param name="shootingPlayer">Player who is shooting (1 or 2)</param>
+        /// <param name="ix">Square on which the player shoots</param>
+        /// <returns>Square content after the shot.</returns>
+        /// <remarks>
+        /// If the shot hits a <see cref="SquareContent.Ship"/> square, the square
+        /// turns into a <see cref="SquareContent.HitShip"/>. 
+        /// </remarks>
+        public SquareContent Shoot(int shootingPlayer, BoardIndex ix)
+        {
+            EnsureValidShooter(shootingPlayer);
+            EnsureValidBoards();
+
+            var board = Boards[shootingPlayer % 2];
+            var shootingBoard = ShootingBoards[shootingPlayer - 1];
+            var content = board[ix];
+            shootingBoard[ix] = content;
+            if (content == SquareContent.Ship)
+            {
+                // We have a hit
+                content = shootingBoard[ix] = SquareContent.HitShip;
+
+                // Check whether the hit sank the ship
+                var shipResult = board.TryFindShip(ix, out var shipRange);
+                if (shipResult == ShipFindingResult.CompleteShip
+                    && shipRange.All(ix => shootingBoard[ix] == SquareContent.HitShip))
+                {
+                    // The hit sank the ship -> change all ship quares to SunkenShip
+                    content = SquareContent.SunkenShip;
+                    foreach(var shipIx in shipRange)
+                    {
+                        shootingBoard[shipIx] = SquareContent.SunkenShip;
+                    }
+                }
+            }
+
+            log.Add(new(PlayerIndexes[shootingPlayer - 1], ix, content));
+            return content;
+        }
+
+        /// <summary>
+        /// Builds a <see cref="ShotRequest"/> from a shot.
+        /// </summary>
+        /// <param name="shootingPlayer"></param>
+        /// <returns></returns>
+        public ShotRequest GetShotRequest(int shootingPlayer)
+        {
+            EnsureValidShooter(shootingPlayer);
+            EnsureValidBoards();
+
+            return new(GameId, PlayerIndexes[shootingPlayer - 1], PlayerIndexes[shootingPlayer % 2], 
+                ShootingBoards[shootingPlayer - 1], GetLastShot(shootingPlayer));
+        }
+
+        public BoardIndex? GetLastShot(int player)
+        {
+            EnsureValidShooter(player);
+            EnsureValidBoards();
+
+            return log.LastOrDefault(l => l.Shooter == PlayerIndexes[player - 1])?.Location;
+        }
+
+        /// <summary>
+        /// Determine winner
+        /// </summary>
+        /// <returns>
+        /// Current winner of the game.
+        /// </returns>
+        public Winner GetWinner(params int[] ships)
+        {
+            EnsureValidBoards();
+            var lostStates = ShootingBoards.Select(b => b.HasLost(ships)).ToArray();
+
+            // A game is considered a draw if more than 200 moves were made
+            if ((lostStates[0] && lostStates[1]) || Log.Count() >= 200)
+            {
+                return Winner.Draw;
+            }
+
+            if (lostStates[0])
+            {
+                return Winner.Player1;
+            }
+
+            if (lostStates[1])
+            {
+                return Winner.Player2;
+            }
+
+            return Winner.NoWinner;
+        }
+    }
+
+    /// <summary>
+    /// Factory for <see cref="Game"/> instances.
+    /// </summary>
+    public interface IGameFactory
+    {
+        /// <summary>
+        /// Create a <see cref="Game"/> instance.
+        /// </summary>
+        /// <param name="player1Index">Index of player 1</param>
+        /// <param name="player2Index">Index of player 2</param>
+        /// <returns>
+        /// New game.
+        /// </returns>
+        Game Create(int player1Index, int player2Index);
+    }
+
+    /// <summary>
+    /// Factory for <see cref="Game"/> instances.
+    /// </summary>
+    /// <remarks>
+    /// The reason for this factory is that it needs a <see cref="IBoardFiller"/>
+    /// from dependency injection.
+    /// </remarks>
+    public class GameFactory : IGameFactory
+    {
+        private readonly IBoardFiller filler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameFactory"/> type.
+        /// </summary>
+        /// <param name="filler">Filler used to fill the game board.</param>
+        public GameFactory(IBoardFiller filler)
+        {
+            this.filler = filler;
+        }
+
+        /// <inheritdoc/>
+        public Game Create(int player1Index, int player2Index)
+        {
+            var boards = new BattleshipBoard[2];
+            for (var i = 0; i < 2; i++)
+            {
+                boards[i] = new BattleshipBoard();
+                filler.Fill(BattleshipBoard.Ships, boards[i]);
+            }
+
+            return new Game(Guid.NewGuid(), new[] { player1Index, player2Index }, boards,
+                new[] { new BoardContent(SquareContent.Unknown), new BoardContent(SquareContent.Unknown) });
+        }
+    }
+}
